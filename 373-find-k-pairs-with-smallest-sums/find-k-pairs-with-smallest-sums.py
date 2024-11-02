cf3cae79896113e5cb0851c612aaf66829fe5761
Kodanda Ravi Kiran <164675643+kodandaravi724@users.noreply.github.com>
2024-11-02 12:54:16 -0400

    Time: 46 ms (99.11%) | Memory: 33.7 MB (92.73%) - LeetSync

--- 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.py
+++ 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.py
@@ -1,22 +1,15 @@
-from heapq import *
+from heapq import heappop, heappush
+
 class Solution:
     def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
-        result: List[List[int]] = []
-        maxHeap = []
-        for i in range(min(k, len(nums1))):
-            for j in range(min(k, len(nums2))):
-                if len(maxHeap) < k:
-                    heappush(maxHeap, (-(nums1[i]+nums2[j]), nums1[i], nums2[j]))
-                else:
-                    currentSum = nums1[i] + nums2[j]
-                    if currentSum < -maxHeap[0][0]:
-                        heappop(maxHeap)
-                        heappush(maxHeap, (-(nums1[i]+nums2[j]), nums1[i], nums2[j]))
-                    else:
-                        break
-        for _, a, b in maxHeap:
-            result.append([a, b])
-        return result
-                
-        
-        
+        res = []
+        queue = [(nums1[0] + nums2[0], 0, 0)]
+        for _ in range(k):
+            _, idx1, idx2 = heappop(queue)
+            res.append([nums1[idx1], nums2[idx2]])
+            if idx2 == 0 and idx1 + 1 < len(nums1):
+                heappush(queue, (nums1[idx1+1] + nums2[0], idx1 + 1, 0))
+
+            if idx2 + 1 < len(nums2):
+                heappush(queue, (nums1[idx1] + nums2[idx2 + 1], idx1, idx2 + 1))
+        return res
