27732b82452f3c9d3639027691befc00b91ac4f3
Kodanda Ravi Kiran <164675643+kodandaravi724@users.noreply.github.com>
2024-11-07 16:33:27 -0500

    Time: 22 ms (66.67%) | Memory: 37.5 MB (5.06%) - LeetSync

---
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.go
@@ -0,0 +1,57 @@
+type Trie struct {
+    children  [26]*Trie
+    isEnd bool
+}
+
+
+func Constructor() Trie {
+    return Trie{}
+}
+
+
+func (this *Trie) Insert(word string)  {
+    node := this
+    for _, char := range word {
+        index := char - 'a'
+        if node.children[index] == nil {
+            newNode := Constructor()
+            node.children[index] = &newNode
+        }
+        node = node.children[index]
+    }
+    node.isEnd = true
+}
+
+
+func (this *Trie) Search(word string) bool {
+    node := this
+    for i := 0; i < len(word); i++ {
+        index := word[i] - 'a'
+        if node.children[index] != nil {
+            node = node.children[index]
+        } else {
+            return false
+        }
+    }
+
+    // fmt.Println("im searching for ", word, node.isEnd)
+    return node.isEnd == true
+}
+
+func init() {
+    debug.SetGCPercent(-1)
+}
+
+
+func (this *Trie) StartsWith(prefix string) bool {
+    node := this
+    for i := 0; i < len(prefix); i++ {
+        index := prefix[i] - 'a'
+        if node.children[index] != nil {
+            node = node.children[index]
+        } else {
+            return false
+        }
+    }
+    return true
+}
