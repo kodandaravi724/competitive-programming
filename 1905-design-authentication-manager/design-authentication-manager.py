6bd6ca633e2eb1bf159718c4faf6dba90f7ceadc
Kodanda Ravi Kiran <164675643+kodandaravi724@users.noreply.github.com>
2025-04-09 19:56:44 -0400

    Time: 15 ms (85.30%) | Memory: 19.5 MB (55.19%) - LeetSync

---
+++ 1905-design-authentication-manager/design-authentication-manager.py
@@ -0,0 +1,30 @@
+import heapq
+
+class AuthenticationManager:
+
+    def __init__(self, timeToLive: int):
+        self.authManager = dict() # tokenId, expiry
+        self.minHeap = [] #(expiryTime, tokenId)
+        self.ttl = timeToLive
+
+
+    def generate(self, tokenId: str, currentTime: int) -> None:
+        tokenExpiry = currentTime + self.ttl
+        self.authManager[tokenId] = tokenExpiry
+        heapq.heappush(self.minHeap, (tokenExpiry, tokenId))
+
+    def renew(self, tokenId: str, currentTime: int) -> None:
+        if tokenId in self.authManager:
+            currExpiryOfToken = self.authManager[tokenId]
+            if currExpiryOfToken > currentTime:
+                newExpiry = currentTime + self.ttl
+                self.authManager[tokenId] = newExpiry
+                heapq.heappush(self.minHeap, (newExpiry, tokenId))
+
+    def countUnexpiredTokens(self, currentTime: int) -> int:
+        while self.minHeap and self.minHeap[0][0] <= currentTime:
+            expiry, tokenId = heapq.heappop(self.minHeap)
+            if tokenId in self.authManager and self.authManager[tokenId] <= currentTime:
+                del self.authManager[tokenId]
+        
+        return len(self.authManager)
