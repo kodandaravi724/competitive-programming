a8e95c9164f3882c4a5dad6ee0e9b5debf7c0d8c
Kodanda Ravi Kiran <164675643+kodandaravi724@users.noreply.github.com>
2025-04-13 11:52:52 -0400

    Time: 60 ms (7.32%) | Memory: 20.3 MB (61.69%) - LeetSync

---
+++ 2352-design-an-atm-machine/design-an-atm-machine.py
@@ -0,0 +1,39 @@
+from collections import defaultdict
+
+class ATM:
+
+    def __init__(self):
+        self.denominations = [20, 50, 100, 200, 500]
+        self.freq = defaultdict(int)
+        
+
+    def deposit(self, banknotesCount: List[int]) -> None:
+        for i, bankNotes in enumerate(banknotesCount):
+            self.freq[self.denominations[i]] += bankNotes
+        
+
+    def withdraw(self, amount: int) -> List[int]:
+        print(self.freq)
+        currentState = self.freq.copy()
+        i = 4
+        res = [0]*5
+        while i >= 0 and amount > 0:
+            curDenomination = self.denominations[i]
+            if self.freq[curDenomination] > 0 and curDenomination <= amount:
+                numDenominations = min(self.freq[curDenomination], (amount // curDenomination))
+                self.freq[curDenomination] -= numDenominations
+                res[i] = numDenominations
+                amount -= numDenominations*curDenomination
+            i -= 1
+        if amount > 0:
+            self.freq = currentState.copy()
+            return [-1]
+        return res
+        
+        
+
+
+# Your ATM object will be instantiated and called as such:
+# obj = ATM()
+# obj.deposit(banknotesCount)
+# param_2 = obj.withdraw(amount)
