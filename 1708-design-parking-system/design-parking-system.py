a451eac87bb6f2137f11156150a27766f33256b3
Kodanda Ravi Kiran <164675643+kodandaravi724@users.noreply.github.com>
2025-06-24 07:55:50 -0400

    Time: 12 ms (68.52%) | Memory: 18.5 MB (28.23%) - LeetSync

--- 1708-design-parking-system/design-parking-system.py
+++ 1708-design-parking-system/design-parking-system.py
@@ -1,13 +1,8 @@
 class ParkingSystem:
-
+    __slots__ = 'slots'
     def __init__(self, big: int, medium: int, small: int):
-        self.spacesRemaining = [big, medium, small] 
+        self.slots = [big, medium, small]
 
     def addCar(self, carType: int) -> bool:
-        self.spacesRemaining[carType-1] -= 1
-        return True if self.spacesRemaining[carType-1] >= 0 else False
-
-
-# Your ParkingSystem object will be instantiated and called as such:
-# obj = ParkingSystem(big, medium, small)
-# param_1 = obj.addCar(carType)
+        self.slots[carType-1] -= 1
+        return self.slots[carType-1] >= 0
